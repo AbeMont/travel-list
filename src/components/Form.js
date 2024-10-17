f8949f2f124f99f2cd3b0e39f0e9163e18c16a0f
Abraham Montoya <abraham.montoya1990@gmail.com>
2024-10-17 13:13:25 -0500

    initial commit

---
+++ src/components/Form.js
@@ -0,0 +1,48 @@
+import {useState} from 'react';
+
+export default function Form({onAddItems}) {
+
+
+  const [description, setDescription] = useState("");
+  const [quantity,setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    // Form check to not add item if descrition is empty
+    if(!description) return;
+
+    const newItem = {id: Date.now(), description, quantity, packed: false}
+    console.log(newItem);
+
+    onAddItems(newItem);
+
+    // Clerar our form after submission
+    setDescription("");
+    setQuantity(1);
+  }
+
+
+  return (
+  <form className="add-form" onSubmit={handleSubmit}>
+    <h3>What do you need for your trip?</h3>
+
+    <select value={quantity} onChange={(e)=> {
+      console.log(e.target.value);
+      setQuantity(Number(e.target.value));
+      }}>
+      {Array.from(
+        {length:20},
+        (_ , i) => i + 1).map(num => <option value={num} key={num}>{num}</option>)}
+    </select>
+
+    <input 
+      type="text" 
+      placeholder="item..." 
+      value={description} 
+      onChange={(e)=> setDescription(e.target.value)}/>
+
+    <button>Add</button>
+  </form>
+  );
+}
